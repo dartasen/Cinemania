fe37e9bccc8ba04772c64a654526f2491b56090d
Clément VARA <clement.VARA@etu.uca.fr>
2018-04-04 09:52:25 +0200

    UI Design

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,6 @@ namespace WpfApp1
     /// </summary>
     public partial class App : Application
     {
+        public string SourceUri => Path.Combine(Directory.GetCurrentDirectory(), "Images/porg.jpg");
     }
 }
